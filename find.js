f3a469869163c6fdea0ac8ff218c17f96b510ef4
slau8 <slau8@stuy.edu>
2017-12-13 13:14:05 -0500

    finished!

--- find.js
+++ find.js
@@ -11,11 +11,14 @@ var box = document.getElementById("box");
 var boxHeight = box.offsetHeight;
 var boxWidth = box.offsetWidth;
 
-//hardcode target as center
-//randomize later
+var body = document.getElementsByTagName("body")[0];
+var heading = document.createElement("h3");
+heading.innerHTML = "CaSh Presents: Find the Invisible Cow";
+body.insertBefore(heading, box);
+
+//random cow location
 var targetX = Math.round(Math.random() * boxWidth);
 var targetY = Math.round(Math.random() * boxHeight);
-
 
 console.log( "box height: " + boxHeight );
 console.log( "box width: " + boxWidth );
@@ -30,34 +33,45 @@ var distance = function (x0, y0, x1, y1) {
 };
 
 var changeColor = function(dist) {
-    var val = Math.round(dist * 255 / (boxHeight * Math.sqrt(2)));
-    console.log("distance: " + dist)
-    if (val > 255)
-	val = 255;
-    val = 255 - val;
+    var val = Math.round(dist * 100 / (boxHeight * Math.sqrt(2)));
+    console.log("distance: " + dist);
+    if (val > 100)
+	val = 100;
+    val = 100 - val;
     //console.log(val)
-    var color = "rgb(" + val + "," + val + "," + val + ")";
+    var color = "hsl(240, 100%, " + val + "%)";
+    //var color = "rgb(" + val + "," + val + "," + val + ")";
     //console.log(color);
     box.setAttribute("style","background-color: " + color);
 };
 
 
 var findIt = function(e) {
-    distance(targetX, targetY, e.clientX, e.clientY);
-    changeColor(dist)
+    distance(targetX, targetY, e.x, e.y);
+    changeColor(dist);
+};
+
+var buttonCallback = function(e){
+    location.reload();
 };
 
 var clickCallback = function(e){
-    if (dist < 50){
-	box.innerHTML = "Hey, you found the cow!"
-	var cow = document.createElement("img")
-	cow.setAttribute("src", "https://vignette.wikia.nocookie.net/harvestmoondscute/images/a/a4/Cow.jpg/revision/latest?cb=20120526004550");
-	box.appendChild(cow);
+    if (dist < 50) {
+	heading.innerHTML = "Hey, you found the cow! "
+	//var cow = document.createElement("img")
+	//cow.setAttribute("src", "https://vignette.wikia.nocookie.net/harvestmoondscute/images/a/a4/Cow.jpg/revision/latest?cb=20120526004550");
+	//heading.appendChild(cow);
+	var button = document.createElement("button");
+	button.innerHTML = "Play again";
+	button.addEventListener("click", buttonCallback);
+	heading.appendChild(button);
     }
-    else{
-	box.innerHTML = "Slau and Cpan present: Find the Cow"
-    }
-}
+};
 
 box.addEventListener("mousemove", findIt);
 box.addEventListener("click", clickCallback);
+
+
+
+
+
